47194a612488e98533929e490d8c8754881ce1a2
Kaan Berkant Atakişi <159454508+KaanBerkantAtakisi@users.noreply.github.com>
2025-01-18 00:01:48 +0300

    Add files via upload

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+
+        public List<Product> Products { get; set; }
+
+    }
+}
+
+/*
+Field-Variable-Property
+ */
+
+/* Eğer ki
+ int x direkt olarak sınıfın içinde tanımlanırsa bu bi field'dır.
+  int x; --> Field
+Eğer ki classın içinde tanımlanan şey sonuna get ve set diye iki tane değer alırsa
+bu bir property oluyor.
+ Eğer ki değişken metod içinde tanımlanıyorsa bu kez değişken oluyor.
+ 
+ */
---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int ProductStock { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public string ProductDescription { get; set; }
+        //Her bir ürünümüzün mutlaka bir kategorisi olmalı
+        public int CategoryId { get; set; }
+
+        //Bir ürünün kategori bilgisine ulaşmam için,kategori tablosunun değerlerine ürün üzerinden ulaşabilmem adına
+
+        public virtual Category Category { get; set; }
+
+        public List<Order> Orders { get; set; }
+
+       // [NotMapped]
+       // public string CategoryName { get; set; }
+
+    }
+}
